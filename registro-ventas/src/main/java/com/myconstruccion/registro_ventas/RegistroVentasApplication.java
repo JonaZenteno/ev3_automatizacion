98022c057cff9152f3706debf1ccd2b18c726cee
JonaZenteno <jon.zenteno@gmail.com>
2024-07-14 16:35:16 -0400

    primer comit

---
+++ registro-ventas/src/main/java/com/myconstruccion/registro_ventas/RegistroVentasApplication.java
@@ -0,0 +1,13 @@
+package com.myconstruccion.registro_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroVentasApplication.class, args);
+	}
+
+}
---
+++ registro-ventas/src/test/java/com/myconstruccion/registro_ventas/RegistroVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myconstruccion.registro_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
